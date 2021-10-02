d8a5e17eb3f7757b836c135b32d1d0b8413b42e0
Xtiru <ghostking1018@gmail.com>
2021-10-02 12:05:42 +0800

    - 新增 Trans files, 於Trans.aspx.cs中添加平方轉坪功能

---
+++ 110-1HW1/Trans.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _110_1HW1
+{
+    public partial class Trans : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            double d_value = 3.982;
+            double sqrf = ((d_value*d_value)*0.3025);
+
+            Response.Write($"d_value = 3.982*3.982 = {sqrf}坪");
+        }
+    }
+}
